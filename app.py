4ac89a611f7fa6edc483e17e3d197e6811f400c8
Sean-Stilwell <sstil051@uottawa.ca>
2024-06-03 11:21:13 -0400

    Updates

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ app.layout = html.Div([
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8080)
+    app.run_server(debug=True, host='0.0.0.0', port=80)
